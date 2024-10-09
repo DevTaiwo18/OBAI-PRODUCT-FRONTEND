b5acc2bbfe5ab7c74b5aca43b3687b11462368f0
DevTaiwo18 <adeyemitaiwo24434@gmail.com>
2024-10-09 12:07:55 +0100

    Added remaining project files

--- vite.config.js
+++ vite.config.js
@@ -4,49 +4,48 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@components': path.resolve('./src/components'),
-            '@utils': path.resolve('./src/utils'),
-            '@constants': path.resolve('./src/constants'),
-            '@images': path.resolve('./src/assets/images'),
-            '@icons': path.resolve('./src/assets/icons'),
-            '@pages': path.resolve('./src/pages'),
-            '@layout': path.resolve('./src/layout'),
-            '@redux': path.resolve('./src/redux')
-            // Add more aliases for other directories as needed
-        }
-    },
-    plugins: [
-        react({
-            jsxRuntime: 'classic' // Using classic JSX runtime
-        })
-    ],
-    esbuild: {
-        loader: 'jsx'
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            loader: {
-                '.js': 'jsx'
-            }
-        }
-    },
-    base: '/',
-
-    // The root directory of the project.
-    root: './',
-
-    // Specify the directory for the built files.
-    build: {
-        outDir: 'dist', // Changed 'build' to 'dist' as Vercel expects 'dist'
-        sourcemap: false,
-        minify: true
-    },
-
-    // Configure the development server.
-    server: {
-        port: 3000,
-        open: true
+  resolve: {
+    alias: {
+      '@components': path.resolve('./src/components'),
+      '@utils': path.resolve('./src/utils'),
+      '@constants': path.resolve('./src/constants'),
+      '@images': path.resolve('./src/assets/images'),
+      '@icons': path.resolve('./src/assets/icons'),
+      '@pages': path.resolve('./src/pages'),
+      '@layout': path.resolve('./src/layout'),
+      '@redux': path.resolve('./src/redux'),
+      // Add more aliases for other directories as needed
     }
+  },
+  plugins: [
+    react({
+      jsxRuntime: 'classic' // Using classic JSX runtime
+    })
+  ],
+  esbuild: {
+    loader: 'jsx'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  },
+  base: './',  // Set base to the current directory to correctly resolve assets
+  // The root directory of the project.
+  root: './',
+
+  // Specify the directory for the built files.
+  build: {
+    outDir: 'dist', // Vercel expects 'dist' directory
+    sourcemap: false,
+    minify: true
+  },
+
+  // Configure the development server.
+  server: {
+    port: 3000,
+    open: true
+  }
 });
