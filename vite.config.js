79b3de0fc05b3134a20a13fa69a85c38a5d1a578
DevTaiwo18 <adeyemitaiwo24434@gmail.com>
2024-10-09 10:37:06 +0100

    Added remaining project files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-// https://vitejs.dev/config/
 export default defineConfig({
     resolve: {
         alias: {
@@ -13,12 +12,11 @@ export default defineConfig({
             '@pages': path.resolve('./src/pages'),
             '@layout': path.resolve('./src/layout'),
             '@redux': path.resolve('./src/redux')
-            // Add more aliases for other directories as needed
         }
     },
     plugins: [
         react({
-            jsxRuntime: 'classic' // Add this line
+            jsxRuntime: 'classic'
         })
     ],
     esbuild: {
@@ -31,21 +29,18 @@ export default defineConfig({
             }
         }
     },
-    base: '/',
+    // Remove or leave out the base path for Vercel deployment
+    // base: './',
 
-    // The root directory of the project.
-    root: './',
-
-    // Specify the directory for the built files.
     build: {
         outDir: 'build',
         sourcemap: false,
         minify: true
     },
 
-    // Configure the development server.
     server: {
         port: 3000,
         open: true
     }
 });
+
