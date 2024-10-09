2cffe9d3a1948d3f30291d19870dd6e05af27bbf
DevTaiwo18 <adeyemitaiwo24434@gmail.com>
2024-10-09 12:33:28 +0100

    Added remaining project files

--- vite.config.js
+++ vite.config.js
@@ -4,42 +4,47 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@components': path.resolve('./src/components'),
-      '@utils': path.resolve('./src/utils'),
-      '@constants': path.resolve('./src/constants'),
-      '@images': path.resolve('./src/assets/images'),
-      '@icons': path.resolve('./src/assets/icons'),
-      '@pages': path.resolve('./src/pages'),
-      '@layout': path.resolve('./src/layout'),
-      '@redux': path.resolve('./src/redux'),
-      // Add more aliases for other directories as needed
-    }
-  },
-  plugins: [
-    react({
-      jsxRuntime: 'classic', // Using classic JSX runtime
-    }),
-  ],
-  esbuild: {
-    loader: 'jsx',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
+    resolve: {
+        alias: {
+            '@components': path.resolve('./src/components'),
+            '@utils': path.resolve('./src/utils'),
+            '@constants': path.resolve('./src/constants'),
+            '@images': path.resolve('./src/assets/images'),
+            '@icons': path.resolve('./src/assets/icons'),
+            '@pages': path.resolve('./src/pages'),
+            '@layout': path.resolve('./src/layout'),
+            '@redux': path.resolve('./src/redux')
+            // Add more aliases for other directories as needed
+        }
     },
-  },
-  base: './', // Set base to current directory to correctly resolve assets in production
-  build: {
-    outDir: 'dist', // Vercel expects the output folder to be 'dist'
-    sourcemap: false,
-    minify: true,
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
+    plugins: [
+        react({
+            jsxRuntime: 'classic' // Using classic JSX runtime
+        })
+    ],
+    esbuild: {
+        loader: 'jsx'
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                '.js': 'jsx'
+            }
+        }
+    },
+    base: './', // Use relative paths for assets
+
+    // Specify the directory for the built files.
+    build: {
+        outDir: 'dist', // Vercel expects 'dist'
+        sourcemap: false,
+        minify: true,
+        assetsInlineLimit: 0 // No limit for inlining assets
+    },
+
+    // Configure the development server.
+    server: {
+        port: 3000,
+        open: true
+    }
 });
